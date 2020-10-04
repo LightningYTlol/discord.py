3004b180c2275dd13441f3f60c77f9f7478de4f9
AceOP <72348287+AceOP5@users.noreply.github.com>
2020-10-04 18:25:12 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+import random
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+
+client.run('BOT_TOKEN')        
