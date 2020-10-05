60fab6d317c6a4841580161761356f828aaade13
AceOP <72348287+AceOP5@users.noreply.github.com>
2020-10-05 12:12:54 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,15 +6,21 @@ client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
-    print('Bot is ready.')
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game('Hello there!'))
+    print("Bot is ready.")
 
 @client.command()
 async def kick(ctx, member : discord.Member, *, reason=None):
     await member.kick(reason=reason)
+    await ctx.send(f'Kicked: {user.mention}')
 
 @client.command()
 async def ban(ctx, member : discord.Member, *, reason=None):
-    await member.ban(reason=reason)
-    
+    await member.kick(reason=reason)   
+    await ctx.send(f'Banned: {user.mention}')
 
-client.run('BOT_TOKEN')        
+@client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)    
+
+client.run('BOT_TOKEN')    
