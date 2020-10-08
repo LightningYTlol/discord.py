9e58a8080820e3593680d22352799a49baf86bfd
AceOP <72348287+AceOP5@users.noreply.github.com>
2020-10-08 13:23:20 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,22 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     await client.change_presence(status=discord.Status.idle, activity=discord.Game('Hello there!'))
     print("Bot is ready.")
+    
+@client.command(pass_context=True)
+async def help(ctx):
+    author = ctx.message.author
+
+    embed = discord.Embed()
+
+    embed.set_author(name='Help')
+    embed.add_field(name='!ban', value='Bans the mentioned user from the server', inline=False)
+    embed.add_field(name='!clear', value='Purges the amount of messages you desire', inline=False)
+    embed.add_field(name='!kick', value='Kicks the mentioned user from the server', inline=False)
+    embed.add_field(name='!mute', value='Mutes the mentioned user, must have the role **muted**', inline=False)
+    embed.add_field(name='!unmute', value='Unmutes the mentioned user', inline=False)
+
+    await author.send(embed=embed)
+    
 
 @client.command()
 async def kick(ctx, member : discord.Member, *, reason=None):
